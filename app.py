e8df826f2698a637fd60717ec1a4f8c5901bac89
Diogo Gomes <dgomes@av.it.pt>
2019-07-19 00:42:39 +0100

    fixes

--- app.py
+++ app.py
@@ -26,7 +26,17 @@ class Camera(object):
             from edgetpu.detection.engine import DetectionEngine
             # Initialize engine.
             self.engine = DetectionEngine("mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite")
+            self.labels = {}
+            with open("coco_labels.txt") as fp:
+                line = fp.readline()
+                cnt = 0 
+                while line:
+                    self.labels[cnt] = line.strip()
+                    line = fp.readline()
+                    cnt += 1
+
             logger.info("%s", self.engine.required_input_array_size())
+            print(self.engine)
         except Exception as err:
             logger.error(err)
 
@@ -43,19 +53,20 @@ class Camera(object):
                             img = frame.to_image()
                             draw = ImageDraw.Draw(img)
                            
-                            specials = [(1300, 100, 200), (1400, 600, 400)]
+                            specials = [(1200, 50, 450), (1400, 600, 400), (900, 700, 300)]
                             for r in specials:
                                 reg = (r[0], r[1], r[0] + r[2], r[1] + r[2])
                                 draw.rectangle(reg, outline='green')
                                 crop_img = img.crop(reg)
-                            
+                                crop_img.thumbnail((300, 300), Image.ANTIALIAS)
                                 if self.engine:
-                                    ans = self.engine.DetectWithImage(crop_img, threshold=0.05, keep_aspect_ratio=True, relative_coord=False, top_k=10)
+                                    ans = self.engine.DetectWithImage(crop_img, threshold=0.20, keep_aspect_ratio=True, relative_coord=False, top_k=10)
                                     for obj in ans:
                                         box = obj.bounding_box.flatten().tolist()
-                                        logger.debug("%s, %s, %s", box, obj.label_id, obj.score)
+                                        box = (box[0]+r[0], box[1]+r[1], int(box[2]*r[2]/300)+r[0], int(box[3]*r[2]/300)+r[1])
+                                        logger.debug("%s, %s, %s", box, self.labels[obj.label_id], obj.score)
                                         draw.rectangle(box, outline='red')
-                                        draw.text((int(box[0]), int(box[1])-24), str(obj.label_id), font=fnt, fill="red")
+                                        draw.text((r[0], r[1]-24), self.labels[obj.label_id], font=fnt, fill="red")
 
                             qcam.append(img)
 
