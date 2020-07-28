fb0c2c9f9e72e54154209b8b5621c52effb54b56
Diogo Gomes <dgomes@ua.pt>
2020-07-28 13:55:52 +0100

    refactor

--- app.py
+++ app.py
@@ -4,75 +4,68 @@ import logging
 import threading
 import time
 from collections import deque
+from itertools import cycle
 
 import av
 from PIL import Image, ImageDraw, ImageFont
 from flask import Flask, render_template, Response
 
+import detect_image
+
 FPS=2 #frames per second in the MJPEG stream
-PF=8 #fraction of frames processed from the stream 1/PF
+PF=8 #fraction of frames processed from the rtsp stream 1/PF
+
+MODEL_FILE = os.getenv('MODEL_FILE', "models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite")
+LABELS_FILE = os.getenv('LABELS_FILE', "models/coco_labels.txt")
 
 DBG_LEVEL = os.getenv('LOGGING_LEVEL', 'INFO').upper()
 logging.basicConfig(level=DBG_LEVEL, format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger(__name__)
 
-qcam = deque(maxlen=24)
 
 class Camera(object):
     def __init__(self, url):
-        self.engine = None
+        self.engine = detect_image.Engine(MODEL_FILE, LABELS_FILE )
         self.rtsp_url = url
-        try:
-            from edgetpu.detection.engine import DetectionEngine
-            # Initialize engine.
-            self.engine = DetectionEngine("mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite")
-            self.labels = {}
-            with open("coco_labels.txt") as fp:
-                line = fp.readline()
-                cnt = 0 
-                while line:
-                    self.labels[cnt] = line.strip()
-                    line = fp.readline()
-                    cnt += 1
+        self._initstate = True
 
-            logger.info("%s", self.engine.required_input_array_size())
-            print(self.engine)
-        except Exception as err:
-            logger.error(err)
+        self.qcam = deque(maxlen=24)
+        self.last_event = deque(maxlen=24)
+
+    def capture_frames(self):
+        rtsp = av.open(self.rtsp_url)
+        fps = 0
+
+        stream = rtsp.streams.video[0]
+        for packet in rtsp.demux(stream):
+            for frame in packet.decode():
+
+                #populate initial image
+                if self._initstate:
+                    img = frame.to_image()
+                    self.qcam.append(img)
+                    self._initstate = False
+
+                fps = (fps+1)%PF
+                if fps == 0:
+                    img = frame.to_image()
+                    
+                    d_img = self.engine.detect_image(img)
+                    if d_img:
+                        self.qcam.append(d_img)
 
     def get_frame(self):
-        fnt = ImageFont.truetype('opensans.ttf', 20)
-        video = av.open(self.rtsp_url)
-        fps = 0
-        for s in video.streams:
-            if s.type == 'video':
-                for packet in video.demux(s):
-                    for frame in packet.decode():
-                        fps = (fps+1)%PF
-                        if fps == 0:
-                            img = frame.to_image()
-                            draw = ImageDraw.Draw(img)
-                           
-                            specials = [(1200, 50, 450), (1400, 600, 400), (900, 700, 300)]
-                            for r in specials:
-                                reg = (r[0], r[1], r[0] + r[2], r[1] + r[2])
-                                draw.rectangle(reg, outline='green')
-                                crop_img = img.crop(reg)
-                                crop_img.thumbnail((300, 300), Image.ANTIALIAS)
-                                if self.engine:
-                                    ans = self.engine.DetectWithImage(crop_img, threshold=0.20, keep_aspect_ratio=True, relative_coord=False, top_k=10)
-                                    for obj in ans:
-                                        box = obj.bounding_box.flatten().tolist()
-                                        box = (box[0]+r[0], box[1]+r[1], int(box[2]*r[2]/300)+r[0], int(box[3]*r[2]/300)+r[1])
-                                        logger.debug("%s, %s, %s", box, self.labels[obj.label_id], obj.score)
-                                        draw.rectangle(box, outline='red')
-                                        draw.text((r[0], r[1]-24), self.labels[obj.label_id], font=fnt, fill="red")
-
-                            qcam.append(img)
+        if len(self.qcam):
+            frame = self.qcam.popleft()
+            self.last_event.append(frame)
+        else:
+            frame = self.last_event.popleft()
+            self.last_event.append(frame)
+        return frame
 
 app = Flask(__name__)
-CAMERA_URL = os.getenv('CAMERA', 'rtsp://admin@192.168.1.96:554/user=admin_password=_channel=0_stream=0.sdp')
 
+CAMERA_URL = os.getenv('CAMERA', 'rtsp://admin@192.168.1.96:554/user=admin_password=_channel=0_stream=0.sdp')
 camera = Camera(CAMERA_URL)  
 
 @app.route('/')
@@ -83,19 +76,19 @@ def gen():
     """Generate MJPEG frames."""
     while True:
         time.sleep(1/FPS)
-        if len(qcam):
-            frame = qcam.popleft()
-            imgByteArr = io.BytesIO()
-            frame.save(imgByteArr, format='JPEG')
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + imgByteArr.getvalue() + b'\r\n')
+        frame = camera.get_frame()
+        imgByteArr = io.BytesIO()
+        frame.save(imgByteArr, format='JPEG')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + imgByteArr.getvalue() + b'\r\n')
+            
 
 @app.route('/live')
 def video_feed():
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    c = threading.Thread(target=camera.get_frame)
+    c = threading.Thread(target=camera.capture_frames)
     c.start()
     app.run(host='0.0.0.0', debug=False)
     c.join()
