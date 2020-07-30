640ebe28c8318902786d1339e86458a3c9b2b615
Diogo Gomes <dgomes@ua.pt>
2020-07-30 17:06:46 +0100

    fix bug

--- app.py
+++ app.py
@@ -95,13 +95,13 @@ class Camera(object):
             img = frame.to_image()
             if self.configuration[CONF_ARMED]:
                 d_img, prob = self.engine.detect_image(img, threshold=self.configuration[CONF_THRESHOLD])
-                self.callback("event_probability", prob)
             else:
                 d_img = img
     
             if d_img:
                 self.event_detected = True
                 self.current_event.append(d_img)
+                self.callback("event_probability", prob)
             else:
                 self.event_detected = False
 
